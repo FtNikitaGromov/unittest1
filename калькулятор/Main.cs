1a97865466b62552343ccbb81e8d4f524d3adffa
FtNikitaGromov <90610084+FtNikitaGromov@users.noreply.github.com>
2021-10-27 09:45:52 +0300

    Update Main.cs

--- калькулятор/Main.cs
+++ калькулятор/Main.cs
@@ -1,73 +1,74 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using calculator;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace calculator.Tests
+namespace calculator
 {
-    [TestClass()]
-    public class CalculatorCheckerTests
+    public class CalculatorChecker
     {
-        [TestMethod()]
-        public void ValidateCalculatorTest_plus_ReturnTrue()
+        public static double ValidateCalculator(double a, double b, string oper)
         {
-            double a = 8;
-            double b = 15;
-            string operation = "+";
-            double result = 23;
-            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
-            Assert.AreEqual(result, actual);
+            if (oper == "+")
+            {
+                return (a + b);
+            }
 
+            if (oper == "-")
+            {
+                return a - b;
+
+            }
+            if (oper == "*")
+            {
+                return (a * b);
+
+            }
+            if (oper == "/")
+            {
+                
+                
+                try
+                {
+
+
+                    if (b == 0)
+                    {
+                        throw new DivideByZeroException();
+
+                        
+                    }
+                    else
+                    { Console.WriteLine("Результат :" + " " + a / b);
+                        return (a / b); }
+                       
+
+                }
+                catch (DivideByZeroException e )
+                {
+                    Console.WriteLine($"Oshibka: {e.Message}");
+                    return 1;
+                }
+
+
+            }
+
+            return 0;
         }
-
-
-        [TestMethod]
-        public void ValidateCalculatorTest_Minus_ReturnTrue()
+        static public void Main()
         {
-            double a = 51;
-            double b = 6;
-            string operation = "-";
-            double result = 45;
-            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
-            Assert.AreEqual(result, actual);
-        }
-
-        [TestMethod]
-        public void ValidateCalculatorTest_multiply_ReturnTrue()
-        {
-            double a = 25;
-            double b = 5;
-            string operation = "*";
-            double result = 125;
-
-            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
-            Assert.AreEqual(result, actual);
-        }
-
-        [TestMethod]
-        public void ValidateCalculatorTest_divide_ReturnTrue()
-        {
-            double a = 7.5;
-            double b = 3;
-            string operation = "/";
-            double result = 2.5;
-
-            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
-            Assert.AreEqual(result, actual);
-        }
-
-        [TestMethod()]
-        [ExpectedException(typeof(DivideByZeroException), "Вы не можете делить на ноль ")]
-        public void ValidateCalculatorTest_divide_ReturnFalse()
-        {
-            double a = 1;
+            double a = 0;
             double b = 0;
 
 
-            string operation = "/";
-            double actual = CalculatorChecker.ValidateCalculator(a, b, operation);
 
+            Console.WriteLine("Введите число 1");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число 2");
+            b = Convert.ToDouble(Console.ReadLine());
+            double res = CalculatorChecker.ValidateCalculator(a, b, "/");
+            Console.WriteLine("res" + res);
         }
 
     }
