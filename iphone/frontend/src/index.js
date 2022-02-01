53c2b7045b16f7f46e8faede3e2f276ca7f6ed5e
FtNikitaGromov <90610084+FtNikitaGromov@users.noreply.github.com>
2022-02-01 12:11:50 +0300

    Create index.js

---
+++ iphone/frontend/src/index.js
@@ -0,0 +1,13 @@
+import "./index.html";
+import "./libs/amenu/amenu.css";
+import "./css/style.css";
+import "./libs/amenu/amenu.js";
+import "./scripts/scroll.js";
+import "./scripts/tabs.js";
+import "./scripts/accordeon.js";
+import "./scripts/getData.js";
+import "./scripts/sendForm.js";
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Securi
